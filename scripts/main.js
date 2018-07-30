c0a2bac76d2520aafd57bef8e20758bac5b09b6d
romankhairov <roman.khairov@gmail.com>
2018-07-30 21:50:38 +0200

    calculation logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 let uah = 7;
 let pln = 1;
-let amount = 100;
+let amount = 200;
+
+const $btnOk = document.querySelector(".btn--ok");
+const $plnInput = document.querySelector("#pln-input");
+const $uahInput = document.querySelector("#uah-input");
 
 
 // const uahPln = amount * uah/pln;
 // console.log(uahPln);
 
 const plnUah = amount * pln/uah;
-console.log(plnUah);
+
 
 //#region Date Block
 
@@ -29,15 +33,39 @@ console.log(today);
 
 //#endregion Date 
 
-
 //#region Converter Block
+
+
+var uahPln = (amount, pln, uah) => amount * pln / uah;
+
 const forUah = uahPln(amount, pln, uah);
 
-function uahPln(amount, pln, uah) {
-    const calc = amount * pln / uah;
-    return calc;
-};
-console.log(forUah);
-console.log(forUah);
 
 //#endregion Converter Block
+
+//#region DOM
+
+$btnOk.addEventListener("click", function(e){
+    e.preventDefault();
+
+    // 1. Select and read data from input
+    const plnAmount = $plnInput.value;
+    const uahAmount = $uahInput.value;
+
+    // 2. Calculate
+
+    if(plnAmount.length !== 0) {
+        const plnValue = plnAmount * pln / uah;
+        console.log(plnValue);
+    } else {
+        const uahValue = uahAmount * uah / pln;
+        console.log(uahValue);
+    }
+    
+    
+    // 3. Output data to UI
+  
+
+});
+
+//#endregion DOM
