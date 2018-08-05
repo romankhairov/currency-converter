a086f29eece2952f310efd325db73732148b5adb
romankhairov <roman.khairov@gmail.com>
2018-08-05 23:38:51 +0200

    revrite loading data

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,3 @@
-let uah = 7;
-let pln = 1;
-let amount = 200;
-
-const $btnOk = document.querySelector(".btn--ok");
-const $plnInput = document.getElementById("pln-input");
-const $uahInput = document.getElementById("uah-input");
-
-$(document).ready(function() {
-    $('select').material_select();
-});
-
-// const uahPln = amount * uah/pln;
-// console.log(uahPln);
-
-const plnUah = amount * pln/uah;
-
-
 //#region Date Block
 
 var today = new Date();
@@ -36,41 +18,19 @@ console.log(today);
 
 //#endregion Date 
 
-//#region Converter Block
+//#region Load Data
 
+function loadData(){
+    const currency1 = document.getElementById('currency1');
+    const currency2 = document.getElementById('currency2');
+    const serverData = new XMLHttpRequest();
+    serverData.onreadystatechange = function() {
+        if(serverData.readyState == 4 && serverData.status == 200) {
+            const obj = JSON.parse(this.responseText);
+            const options = '';
+            for(key in obj.rates);
+        }
+    }
+}
 
-var uahPln = (amount, pln, uah) => amount * pln / uah;
-
-const forUah = uahPln(amount, pln, uah);
-
-
-//#endregion Converter Block
-
-//#region DOM
-
-$btnOk.addEventListener("click", function(e){
-    e.preventDefault();
-
-    // 1. Select and read data from input
-    const plnAmount = ~~parseInt($plnInput.value); // use "~~" for convert string into 0
-    const uahAmount = ~~parseInt($uahInput.value);
-
-    
-    // (typeof plnAmount !== "NaN") ? (plnAmount * pln / uah) : (console.log("Enter a number!"));
-
-    // 2. Calculate
-
-    if(plnAmount.length !== 0) {
-        $uahInput.value = plnAmount * pln / uah;
-    } else {
-        $plnInput.value = uahAmount * uah / pln;
-    } 
-
-    
-    // 3. Output data to UI
-  
-
-
-});
-
-//#endregion DOM
+//#endregion Load Data
