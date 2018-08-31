85ff77ae4f960cd2f3f4c2d878108b49ff33818f
romankhairov <roman.khairov@gmail.com>
2018-08-31 22:11:17 +0200

    save

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
 //#region Date Block
 
-// var today = new Date();
-// var dd = today.getDate();
-// var mm = today.getMonth()+1; //January is 0!
-// var yyyy = today.getFullYear();
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
 
-// if(dd<10) {
-//     dd = '0'+ dd
-// } 
+if(dd<10) {
+    dd = '0'+ dd
+} 
 
-// if(mm<10) {
-//     mm = '0'+ mm
-// } 
+if(mm<10) {
+    mm = '0'+ mm
+} 
 
-// today = `${mm}/${dd}/${yyyy}`;
-// console.log(today);
+today = `${mm}/${dd}/${yyyy}`;
+console.log(today);
 
 //#endregion Date 
 
